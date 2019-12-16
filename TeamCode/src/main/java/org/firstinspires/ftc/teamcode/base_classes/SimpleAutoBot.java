3ed22c92ba671128e6cc5d24f3aa5649b65c1cd6
c20mp <c20m>
2019-12-16 11:37:08 -0500

    Separated complex and simple auto robots
    Signed-off-by: c20mp <c20m>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base_classes/SimpleAutoBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base_classes/SimpleAutoBot.java
@@ -2,10 +2,8 @@ package org.firstinspires.ftc.teamcode.base_classes;
 
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.algorithms.IMU;
 import org.firstinspires.ftc.teamcode.algorithms.VuforiaAutoNav;
@@ -17,16 +15,14 @@ import static org.firstinspires.ftc.teamcode.enums.Direction.LEFT;
 import static org.firstinspires.ftc.teamcode.enums.Direction.RIGHT;
 
 /**
- * This class is the class for complex autonomous robots
+ * This class is the class for simple autonomous robots
  * <p>
  * TODO: implement bound checks for robot (based on math for size of robot)
  */
-public class AutoBot extends Robot {
+public class SimpleAutoBot extends Robot {
 
     //stores the value of sin(45°), or sin(pi/4)
     public double sin45 = Math.sqrt(2) / 2;
-    private VuforiaAutoNav nav;
-    private IMU imu;
     private float robotWidth = 15.68f; // robot width in inches
     private float robotLength = 16.25f; // robot length in inches
     //Declares encoder numbers
@@ -37,46 +33,7 @@ public class AutoBot extends Robot {
     final double LIFT_COUNTS_PER_INCH = 5000;
     private int alliance = 1;
 
-    private float x = 0;
-    private float y = 0;
     private ElapsedTime runtime = new ElapsedTime();
-
-    /**
-     * gets the x
-     *
-     * @return x
-     */
-    public float getX() {
-        return x;
-    }
-
-    /**
-     * sets the x
-     *
-     * @param x
-     */
-    public void setX(float x) {
-        this.x = x;
-    }
-
-    /**
-     * gets the y
-     *
-     * @return y
-     */
-    public float getY() {
-        return y;
-    }
-
-    /**
-     * sets the y
-     *
-     * @param y
-     */
-    public void setY(float y) {
-        this.y = y;
-    }
-
 
     /**
      * gets the width
@@ -142,7 +99,7 @@ public class AutoBot extends Robot {
      *
      * @param opMode
      */
-    public AutoBot(LinearOpMode opMode) {
+    public SimpleAutoBot(LinearOpMode opMode) {
         this.opMode = opMode;
     }
 
@@ -153,78 +110,6 @@ public class AutoBot extends Robot {
      */
     public double[] getPowers() {
         return new double[]{frontLeft.getPower(), frontRight.getPower(), backRight.getPower(), backLeft.getPower()};
-    }
-
-    /**
-     * gets the auto nav
-     *
-     * @return auto nav
-     */
-    public VuforiaAutoNav getNav() {
-        return nav;
-    }
-
-    /**
-     * sets the auto nav
-     *
-     * @param nav
-     */
-    public void setNav(VuforiaAutoNav nav) {
-        this.nav = nav;
-    }
-
-    /**
-     * gets the imu
-     *
-     * @return imu
-     */
-    public IMU getIMU() {
-        return imu;
-    }
-
-    /**
-     * sets the imu
-     *
-     * @param imu
-     */
-    public void setIMU(IMU imu) {
-        this.imu = imu;
-    }
-
-    /**
-     * gets the nav angle
-     *
-     * @return angle
-     */
-    public float getNavAngle() {
-        return this.getNav().getRobotAngle();
-    }
-
-    /**
-     * sets the nav angle
-     *
-     * @param angle`
-     */
-    public void setNavAngle(float angle) {
-        this.getNav().setRobotAngle(angle);
-    }
-
-    /**
-     * gets the imu angle
-     *
-     * @return angle
-     */
-    public float getIMUAngle() {
-        return this.getIMU().getAngle();
-    }
-
-    /**
-     * sets the imu angle
-     *
-     * @param angle
-     */
-    public void setIMUAngle(float angle) {
-        this.getIMU().setAngle(angle);
     }
 
     /**
@@ -246,121 +131,12 @@ public class AutoBot extends Robot {
     }
 
     /**
-     * changes the coordinates as if the robot was moved for a distance in a direction (based on current angle)
-     *
-     * @param distance distance to move
-     * @param dir      direction to move in
-     */
-    public void moveRobot(float distance, Direction dir) {
-        if (dir == FORWARD) {
-            this.setX(this.getX() + (float) (distance * Math.sin((double) this.getIMUAngle())));
-            this.setY(this.getY() + (float) (distance * Math.cos((double) this.getIMUAngle())));
-        } else if (dir == LEFT) {
-            this.setX(this.getX() + (float) (distance * Math.sin((double) this.getIMUAngle() + 90)));
-            this.setY(this.getY() + (float) (distance * Math.cos((double) this.getIMUAngle() + 90)));
-        } else if (dir == BACKWARD) {
-            this.setX(this.getX() + (float) (distance * Math.sin((double) this.getIMUAngle() + 180)));
-            this.setY(this.getY() + (float) (distance * Math.cos((double) this.getIMUAngle() + 180)));
-        } else if (dir == RIGHT) {
-            this.setX(this.getX() + (float) (distance * Math.sin((double) this.getIMUAngle() + 270)));
-            this.setY(this.getY() + (float) (distance * Math.cos((double) this.getIMUAngle() + 270)));
-        }
-
-    }
-
-    /**
      * Initializes the robot (Use this instead of .init())
      */
     public void initAuto() {
         this.init();
-        this.setNav(new VuforiaAutoNav(this.opMode.hardwareMap, this.opMode.telemetry));
-        this.setIMU(new IMU(this.opMode.hardwareMap, this.opMode.telemetry));
-        this.getNav().initView();
-        this.getIMU().initIMU();
-        this.getNav().updateView();
-        this.setX(this.getNav().getRobotX());
-        this.setY(this.getNav().getRobotY());
-        this.getIMU().setAngle(this.getNavAngle());
     }
 
-    /**
-     * Rotates using the IMU in a direction for an angle
-     *
-     * @param speed   the speed (power) to use for the rotation
-     * @param degrees angle in degrees to rotate (between 0 and 359)
-     * @param timeout max time driving before stopping in seconds
-     * @param dir     direction (1 is clockwise, -1 is counterclockwise)
-     */
-    public void IMURotateForAngle(double speed, double degrees, double timeout, int dir) {
-
-        double target = 0;
-
-        if (this.opMode.opModeIsActive()) {
-
-            // Set target
-            target = this.getIMU().getAngles().firstAngle + degrees * dir;
-
-            // Reset the timeout time and start motion.
-            this.getRunTime().reset();
-            this.rotateCounter(speed * dir);
-        }
-
-        // Keep looping while we are still active, and there is time left, and both motors are running.
-        while (this.opMode.opModeIsActive() && (this.getRunTime().seconds() < timeout) && dir * (target - this.getIMU().getAngles().firstAngle) > 5) {
-
-            // Display it for the driver.
-            this.opMode.telemetry.addData("Path1", "Running to %7d", target);
-            this.opMode.telemetry.update();
-        }
-
-        // Stop all motion
-        this.stopDriving();
-
-        this.opMode.sleep(250);   // optional pause after each move
-    }
-
-    /**
-     * Rotates using the IMU in a direction to an angle
-     *
-     * @param speed   the speed (power) to use for the rotation
-     * @param degrees angle in degrees to rotate to (between 0 and 359)
-     * @param timeout max time driving before stopping in seconds
-     */
-    public void IMURotateToAngle(double speed, double degrees, double timeout) {
-
-        double target = degrees;
-        int dir = 0;
-        if (Math.abs(target - this.getIMU().getAngles().firstAngle) < 180) {
-            dir = (int) Math.signum(target - this.getIMU().getAngles().firstAngle);
-        } else {
-            dir = -1 * (int) Math.signum(target - this.getIMU().getAngles().firstAngle);
-            dir = -1 * (int) Math.signum(target - this.getIMU().getAngles().firstAngle);
-        }
-        if (dir != -1 && dir != 1) {
-            dir = 1;
-        }
-
-        if (this.opMode.opModeIsActive()) {
-
-
-            // Reset the timeout time and start motion.
-            this.getRunTime().reset();
-            this.rotateCounter(speed * dir);
-        }
-
-        // Keep looping while we are still active, and there is time left, and both motors are running.
-        while (this.opMode.opModeIsActive() && (this.getRunTime().seconds() < timeout) && dir * (target - this.getIMU().getAngles().firstAngle) > 5) {
-
-            // Display it for the driver.
-            this.opMode.telemetry.addData("Path1", "Running to %7d", target);
-            this.opMode.telemetry.update();
-        }
-
-        // Stop all motion
-        this.stopDriving();
-
-        this.opMode.sleep(250);   // optional pause after each move
-    }
 
     /**
      * LIFT
@@ -447,7 +223,6 @@ public class AutoBot extends Robot {
 
         // Stop all motion
         this.stopDriving();
-        this.moveRobot((float) (Math.abs(this.frontLeft.getCurrentPosition() - current) / COUNTS_PER_INCH), dir);
 
         // Turn off RUN_TO_POSITION
         this.frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SimpleAutoMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SimpleAutoMain.java
@@ -17,6 +17,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 import org.firstinspires.ftc.teamcode.base_classes.AutoBot;
+import org.firstinspires.ftc.teamcode.base_classes.SimpleAutoBot;
 import org.firstinspires.ftc.teamcode.enums.Alliance;
 import org.firstinspires.ftc.teamcode.enums.Direction;
 
@@ -31,40 +32,23 @@ public abstract class SimpleAutoMain extends LinearOpMode {
 
     public abstract Alliance getAlliance();
 
-    public AutoBot robot = new AutoBot(this);
+    public SimpleAutoBot robot = new SimpleAutoBot(this);
     public ElapsedTime runtime = new ElapsedTime();
 
     //Declares encoder numbers
 
     @Override
     public void runOpMode() {
-/*        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
-        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
-        parameters.loggingEnabled = true;
-        parameters.loggingTag = "IMU";
-        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
-
-        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
-        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
-        // and named "imu".
-        imu = hardwareMap.get(BNO055IMU.class, "imu");
-        imu.initialize(parameters);
-//
-//        telemetry.addData("Status", "Initialized");
-//        telemetry.addData("Alliance", getAlliance());
-//        telemetry.addData("Test", "test");
-//
-//        composeTelemetry();
-//
-//        telemetry.update();*/
+        telemetry.addData("Status", "Initialized");
+        telemetry.addData("Alliance", getAlliance());
+        telemetry.addData("Test", "test");
+        telemetry.update();
 
         robot.initAuto();
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
-        runtime.reset();
+        this.robot.getRunTime().reset();
 
 
         // Run until the end of auto (driver presses STOP)
@@ -91,250 +75,15 @@ public abstract class SimpleAutoMain extends LinearOpMode {
 
             //Park
             //TODO: Change these inches
-            if (getAlliance() == Alliance.BLUE) this.robot.encoderDrive(.7, 15, 10, Direction.BACKWARD);
-            else if (getAlliance() == Alliance.RED) this.robot.encoderDrive(.7, 15, 10, Direction.FORWARD);
+            if (getAlliance() == Alliance.BLUE)
+                this.robot.encoderDrive(.7, 15, 10, Direction.BACKWARD);
+            else if (getAlliance() == Alliance.RED)
+                this.robot.encoderDrive(.7, 15, 10, Direction.FORWARD);
 
 
         }
 
     }
 
-/*    void composeTelemetry() {
 
-        // At the beginning of each telemetry update, grab a bunch of data
-        // from the IMU that we will then display in separate lines.
-        telemetry.addAction(new Runnable() {
-            @Override
-            public void run() {
-                // Acquiring the angles is relatively expensive; we don't want
-                // to do that in each of the three items that need that info, as that's
-                // three times the necessary expense.
-                angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-                gravity = imu.getGravity();
-            }
-        });
-
-        telemetry.addLine()
-                .addData("status", new Func<String>() {
-                    @Override
-                    public String value() {
-                        return imu.getSystemStatus().toShortString();
-                    }
-                })
-                .addData("calib", new Func<String>() {
-                    @Override
-                    public String value() {
-                        return imu.getCalibrationStatus().toString();
-                    }
-                });
-
-        telemetry.addLine()
-                .addData("heading", new Func<String>() {
-                    @Override
-                    public String value() {
-                        return formatAngle(angles.angleUnit, angles.firstAngle);
-                    }
-                })
-            .addData("roll", new Func<String>() {
-                @Override public String value() {
-                    return formatAngle(angles.angleUnit, angles.secondAngle);
-                    }
-                })
-            .addData("pitch", new Func<String>() {
-                @Override public String value() {
-                    return formatAngle(angles.angleUnit, angles.thirdAngle);
-                    }
-                });
-
-        telemetry.addLine()
-                .addData("grvty", new Func<String>() {
-                    @Override
-                    public String value() {
-                        return gravity.toString();
-                    }
-                })
-                .addData("mag", new Func<String>() {
-                    @Override
-                    public String value() {
-                        return String.format(Locale.getDefault(), "%.3f",
-                                Math.sqrt(gravity.xAccel * gravity.xAccel
-                                        + gravity.yAccel * gravity.yAccel
-                                        + gravity.zAccel * gravity.zAccel));
-                    }
-                });
-    }
-
-    //----------------------------------------------------------------------------------------------
-    // Formatting
-    //----------------------------------------------------------------------------------------------
-
-    String formatAngle(AngleUnit angleUnit, double angle) {
-        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
-    }
-
-    String formatDegrees(double degrees) {
-        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
-    }*/
-
-//    /**
-//     * Rotates using the IMU in a direction for an angle
-//     *
-//     * @param speed   the speed (power) to use for the rotation
-//     * @param degrees angle in degrees to rotate (between -180 and 180)
-//     * @param timeout max time driving before stopping in seconds
-//     * @param dir     direction (1 is clockwise, -1 is counterclockwise)
-//     */
-/*    public void IMURotateForAngle(double speed, double degrees, double timeout, int dir) {
-
-        double target = 0;
-
-        if (opModeIsActive()) {
-
-            // Set target
-            target = angles.firstAngle + degrees * dir;
-
-            // Reset the timeout time and start motion.
-            runtime.reset();
-            robot.rotateClockwise(speed * dir);
-        }
-
-        // Keep looping while we are still active, and there is time left, and both motors are running.
-        while (opModeIsActive() && (runtime.seconds() < timeout) && dir * (target - angles.firstAngle) > 5) {
-
-            // Display it for the driver.
-            telemetry.addData("Path1", "Running to %7d", target);
-            telemetry.update();
-        }
-
-        // Stop all motion
-        robot.stopDriving();
-
-        sleep(250);   // optional pause after each move
-    }*/
-
-//    /**
-//     * Rotates using the IMU in a direction to an angle
-//     *
-//     * @param speed   the speed (power) to use for the rotation
-//     * @param degrees angle in degrees to rotate to (between -180 and 180)
-//     * @param timeout max time driving before stopping in seconds
-//     */
-/*    public void IMURotateToAngle(double speed, double degrees, double timeout) {
-
-        double target = degrees;
-        int dir = 0;
-        if (Math.abs(target - angles.firstAngle) < 180) {
-            dir = (int) Math.signum(target - angles.firstAngle);
-        } else {
-            dir = -1 * (int) Math.signum(target - angles.firstAngle);
-        }
-        if (dir != -1 && dir != 1) {
-            dir = 1;
-        }
-
-        if (opModeIsActive()) {
-
-
-            // Reset the timeout time and start motion.
-            runtime.reset();
-            robot.rotateClockwise(speed * dir);
-        }
-
-        // Keep looping while we are still active, and there is time left, and both motors are running.
-        while (opModeIsActive() && (runtime.seconds() < timeout) && dir * (target - angles.firstAngle) > 5) {
-
-            // Display it for the driver.
-            telemetry.addData("Path1", "Running to %7d", target);
-            telemetry.update();
-        }
-
-        // Stop all motion
-        robot.stopDriving();
-
-        sleep(250);   // optional pause after each move
-    }*/
-
-//    /**
-//     * LIFT
-//     */
-/*    public void encoderLift(double speed, double counts, double timeout, Direction dir) {
-        int target = 0;
-
-        if (opModeIsActive()) {
-
-            // Set target
-            target = robot.lift.getCurrentPosition() + (int) counts;
-            if (dir == Direction.DOWN) target = -target;
-
-            // Turn On RUN_TO_POSITION
-            robot.lift.setTargetPosition(target);
-            robot.lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            // Reset the timeout time and start motion.
-            runtime.reset();
-            if (dir == Direction.UP) robot.raiseLift(speed);
-            else if (dir == Direction.DOWN) robot.lowerLift(speed);
-
-        }
-
-        // Keep looping while we are still active, and there is time left, and both motors are running.
-        while (opModeIsActive() && (runtime.seconds() < timeout) && (robot.lift.isBusy())) {
-
-            // Display it for the driver.
-            telemetry.addData("Path1", "Lift Running to %7d", target);
-            telemetry.addData("Path2", "Lift Running at %7d", robot.lift.getCurrentPosition());
-            telemetry.update();
-        }
-
-        // Stop lift
-        robot.stopLift();
-
-        // Turn off RUN_TO_POSITION
-        robot.lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        sleep(250);   // optional pause after each move
-
-    }
-
-    public void encoderDrive(double speed, double inches, double timeout, Direction dir) {
-
-        int target = 0;
-
-        if (opModeIsActive()) {
-
-            // Set target
-            target = robot.frontLeft.getCurrentPosition() + (int) (inches * COUNTS_PER_INCH);
-
-            // Turn On RUN_TO_POSITION
-            if (dir == Direction.BACKWARD) target = -target;
-            else if (dir == Direction.RIGHT) target = -target;
-            robot.frontLeft.setTargetPosition(target);
-            robot.frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            // Reset the timeout time and start motion.
-            runtime.reset();
-            if (dir == Direction.FORWARD) robot.driveForwards(speed);
-            else if (dir == Direction.BACKWARD) robot.driveBackwards(speed);
-            else if (dir == Direction.RIGHT) robot.strafeRight(speed);
-            else if (dir == Direction.LEFT) robot.strafeLeft(speed);
-
-        }
-
-        // Keep looping while we are still active, and there is time left, and both motors are running.
-        while (opModeIsActive() && (runtime.seconds() < timeout) && (robot.frontLeft.isBusy())) {
-
-            // Display it for the driver.
-            telemetry.addData("Path1", "Running to %7d", target);
-            telemetry.addData("Path2", "Running at %7d", robot.frontLeft.getCurrentPosition());
-            telemetry.update();
-        }
-
-        // Stop all motion
-        robot.stopDriving();
-
-        // Turn off RUN_TO_POSITION
-        robot.frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        sleep(250);   // optional pause after each move
-    }*/
 }
